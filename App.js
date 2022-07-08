995541f960aba749206d75c918a8f2faa3ced99e
Unche Emmanuel <emydukeifoulimre@gmail.com>
2022-07-08 17:13:03 +0100

    project created and dependencies installed

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+
+  }
+
+
+  render() {
+    return (
+      <View>
+        <Text>Hi, hello</Text>
+      </View>
+    )
+
+  }
+
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
